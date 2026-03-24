cbc49023f36b0ac950c30e88959793c429f1767b
Abhishek102-tech <181866251+Abhishek102-tech@users.noreply.github.com>
2026-03-25 00:03:16 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <header className='heading'><div className="settings"></div>Block Busters</header>
+    </>
+  )
+}
+
+export default App
